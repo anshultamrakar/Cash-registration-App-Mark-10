6c067ca3941dfdd3a4e270ab81530d862cbc6805
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-14 15:07:20 -0400

    fix
    All bugs are fixed

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ const noOfNotes = document.querySelectorAll(".no-Of-Notes");
 const availableNotes = [2000,500,100,20,5,1];
 
 buttonCheck.addEventListener("click", function validateBillAmount (){
-hideMessage();
+
+    hideMessage();
    if(billAmount.value > 0 ){
 if (cashGiven.value >= billAmount.value){
    const amountToBeReturned = cashGiven.value - billAmount.value ;
+
    calculateChange(amountToBeReturned);
 }else{
     showMessage("Do you want to wash the Plates") ;
@@ -25,10 +27,9 @@ function hideMessage(){
     message.style.display ="none" ;
 }
 
-function calculateChange(){
+function calculateChange(amountToBeReturned){
 for (let i = 0; i < availableNotes.length ; i++){
-    const numberOfNotes = Math.trunc(amountToBeReturned/ availableNotes[i]
-        );
+    const numberOfNotes = Math.trunc(amountToBeReturned/ availableNotes[i]);
         amountToBeReturned %= availableNotes[i]
         noOfNotes[i].innerText = numberOfNotes;
 }
